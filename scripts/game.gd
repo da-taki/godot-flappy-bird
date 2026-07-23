484fc5f34afb83ee01c73ac9578e225a0cafc7a6
da-taki <singhtaknoor@gmail.com>
2026-07-24 02:59:20 +0530

    Added Score

--- scripts/game.gd
+++ scripts/game.gd
@@ -6,17 +6,31 @@ const PILLAR_SCENE: PackedScene = preload("res://scenes/pillar_pair.tscn")
 @export var minimum_spawn_y: float = -160.0
 @export var maximum_spawn_y: float = 160.0
 
+var score: int = 0
+
+@onready var score_label: Label = $UI/ScoreLabel
+
 func _ready() -> void:
+	update_score_label()
 	spawn_pillar()
 	
 func spawn_pillar() -> void: 
 	var pillar := PILLAR_SCENE.instantiate() as Node2D
 	
 	add_child(pillar)
+	pillar.connect("scored", add_score)
 	
 	pillar.position = Vector2(
 		spawn_x,
 		randf_range(minimum_spawn_y, maximum_spawn_y)
 	)
+	
+func add_score() -> void:
+	score += 1
+	update_score_label()
+	
+func update_score_label() -> void:
+	score_label.text = str(score)
+	
 func _on_pillar_spawn_timer_timeout() -> void:
 	spawn_pillar()
--- scripts/pillar_pair.gd
+++ scripts/pillar_pair.gd
@@ -1,9 +1,21 @@
 extends Node2D
 
+signal scored
+
 @export var speed: float = 180.0
+
+var has_scored: bool = false
 
 func _physics_process(delta: float) -> void:
 	position.x -= delta * speed
 	
 	if global_position.x < -610:
 		queue_free()
+
+func _on_score_zone_body_entered(body: Node2D) -> void:
+	if has_scored:
+		return
+		
+	if body is CharacterBody2D:
+		has_scored = true
+		scored.emit()
