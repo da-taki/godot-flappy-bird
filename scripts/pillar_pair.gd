f38c4b099a05ca28bc818c5e57dac39b3bff2ce0
da-taki <singhtaknoor@gmail.com>
2026-07-24 04:52:41 +0530

    Added loosing screen and the ability to loose

--- scripts/pillar_pair.gd
+++ scripts/pillar_pair.gd
@@ -5,6 +5,7 @@ signal scored
 @export var speed: float = 180.0
 
 var has_scored: bool = false
+var has_lost: bool = false
 
 func _physics_process(delta: float) -> void:
 	position.x -= delta * speed
@@ -19,3 +20,11 @@ func _on_score_zone_body_entered(body: Node2D) -> void:
 	if body is CharacterBody2D:
 		has_scored = true
 		scored.emit()
+		
+func _on_lose_upper_body_entered(body: Node2D) -> void:
+	if body is CharacterBody2D:
+		get_tree().change_scene_to_file("res://scenes/lose_screen.tscn")
+
+func _on_lose_lower_body_entered(body: Node2D) -> void:
+	if body is CharacterBody2D:
+		get_tree().change_scene_to_file("res://scenes/lose_screen.tscn")
