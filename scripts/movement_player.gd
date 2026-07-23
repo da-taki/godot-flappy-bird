b4447318a294ef024677d603642a9dd335899e7c
da-taki <singhtaknoor@gmail.com>
2026-07-24 01:18:33 +0530

    Added player movement

--- scripts/movement_player.gd
+++ scripts/movement_player.gd
@@ -1,1 +1,12 @@
 extends CharacterBody2D
+
+const GRAVITY = 700.0
+const JUMP_FORCE = -350.0
+
+func _physics_process(delta: float) -> void:
+	velocity.y += delta * GRAVITY
+	
+	if Input.is_action_just_pressed("ui_accept"):
+		velocity.y = JUMP_FORCE
+		
+	move_and_slide()
